2e48f81446d348c391d687e6007f653bb1af6560
Abir Hasan Dipu <mdabirhasandipu@gmail.com>
2022-05-03 02:39:42 +0600

    Restaurant page start

--- reviewsystem/resources/views/restaurant.blade.php
+++ reviewsystem/resources/views/restaurant.blade.php
@@ -2,3 +2,89 @@
 
 <script src="{{ asset('js/app.js') }}" defer></script>
 
+<div class="mx-64 grid grid-cols-3 gap-10">
+    <!-- card 1 -->
+    <a href="#">
+        <div class="col-span-1 bg-white h-40 ">
+            <div class="">
+                <div class="">
+                    <img class="object-cover h-52 w-80" src="../../public/images/ff.jpg" alt="">
+                    <div class="absolute">
+                        <p class="m-2 text-white bg-sky-600 rounded-2xl px-2 py-1 font-semibold text-md">3.6 <i class="fa-solid fa-star"></i></i></p>
+                    </div>
+                </div>
+                <h3 class="p-1 text-center text-xl">Testy Treat</h3>
+
+            </div>
+        </div>
+    </a>
+    <!-- card 2 -->
+    <a href="#">
+        <div class="col-span-1 bg-white ">
+            <div class="">
+                <div class="">
+                    <img class="object-cover h-52 w-80" src="../../public/images/img2.jpg" alt="">
+                </div>
+                <h3 class="p-1 text-center text-xl">Indian Spicy Chicken</h3>
+            </div>
+        </div>
+    </a>
+    <!-- card 3 -->
+    <a href="#">
+        <div class="col-span-1 bg-white">
+            <div class="">
+                <div class="">
+                    <img class="object-cover h-52 w-80" src="../../public/images/ff.jpg" alt="">
+                </div>
+                <h3 class="p-1 text-center text-xl">Tommy Miah's Tea Bar</h3>
+            </div>
+        </div>
+    </a>
+    <!-- card 4 -->
+    <a href="#">
+        <div class="col-span-1 bg-white ">
+            <div class="">
+                <div class="">
+                    <img class="object-cover h-52 w-80" src="../../public/images/img2.jpg" alt="">
+                </div>
+                <h3 class="p-1 text-center text-xl">Bachelors'</h3>
+            </div>
+        </div>
+    </a>
+
+    <!-- card 5 -->
+    <a href="#">
+        <div class="col-span-1 bg-white ">
+            <div class="">
+                <div class="">
+                    <img class="object-cover h-52 w-80" src="../../public/images/img3.jpg" alt="">
+                </div>
+                <h3 class="p-1 text-center text-xl">Cafe Gulshan</h3>
+            </div>
+        </div>
+    </a>
+
+    <!-- card 6 -->
+    <a href="#">
+        <div class="col-span-1 bg-white ">
+            <div class="">
+                <div class="">
+                    <img class="object-cover h-52 w-80" src="../../public/images/img3.jpg" alt="">
+                </div>
+                <h3 class="p-1 text-center text-xl">Kalapata Restaurant</h3>
+            </div>
+        </div>
+    </a>
+
+    <!-- card 7 -->
+    <a href="#">
+        <div class="col-span-1 bg-white ">
+            <div class="">
+                <div class="">
+                    <img class="object-cover h-52 w-80" src="../../public/images/img3.jpg" alt="">
+                </div>
+                <h3 class="p-1 text-center text-xl">Marwa</h3>
+            </div>
+        </div>
+    </a>
+</div>
